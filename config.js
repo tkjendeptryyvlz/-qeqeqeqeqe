1e070290bbca34b95a6dc683b71b76dd8200485d
tkjendeptryyvlz <86883724+tkjendeptryyvlz@users.noreply.github.com>
2021-07-03 21:56:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
 const CONFIG = {
     introTitle: 'heyyy! chào người anh em tôi thấy bạn có vẻ hơi bị sờ chét nhỡ',
     introDesc: `còn vài ngày nữa đến kì thi có thể nói là quan trọng nhất trong thời đi học 😚😚
-    nên là tôi biết bạn có vẻ áp lức lắm😘',
+ nên là tôi biết bạn có vẻ áp lức lắm😘
+cố lên nhé
+    cố lên?',
     btnIntro: 'mình muốn nói với bạn là ^^!',
     title: 'hãy tiếp tục cho dù kết quả như thế nào đi nữa bạn đã cố gắn hết sức rồi  😙',
     desc: 'cố lên bạn tôi đừng suy nghĩ phức tạp hóa vấn đề lên nhé 😙',
     btnYes: 'tiếp tục cố gắn đi bạn tôi',
     btnNo: 'đừng suy nghĩ quá nhiều nữa nào:3',
-    question:'Mình có đôi lời chúc đến bạn ',
+    question:'Mình có đôi lời chúc đến bạn',
     btnReply: 'tôi biết bạn mệt mõi và áp lực lắp như mà cố lên nhé!!',
     reply: 'Miệt mài suốt bao đêm thêu dệt những ước mơ, hãy biến ước muốn của bạn thành hiện thực. Thành công và hạnh phúc sẽ luôn đồng hành cùng bạn',
     mess: 'tôi biết các bạn mệt mõi mà áp lức lặp như mà cố lên !!',
