7f5512627e6d4efafd607e7ab9fe79d497f1a6f6
tkjendeptryyvlz <86883724+tkjendeptryyvlz@users.noreply.github.com>
2021-07-03 21:39:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'heyyy! chào người anh em tôi thấy bạn có vẻ hơi bị sờ chét nhỡ',
+    introDesc: `còn vài ngày nữa đến kì thi có thể nói là quan trọng nhất trong thời đi học 😚😚
+    nên là tôi biết bạn có vẻ áp lức lắm😘`
+    btnIntro: 'mình muốn nói với bạn là ^^!',
+    title: 'hãy tiếp tục cho dù kết quả như thế nào đi nữa bạn đã cố gắn hết sức rồi  😙',
+    desc: 'cố lên bạn tôi đừng suy nghĩ phức tạp hóa vấn đề lên nhé 😙',
+    btnYes: 'tiếp tục cố gắn đi bạn tôi',
+    btnNo: 'đừng suy nghĩ quá nhiều nữa nào:3',
+    question:'Mình có đôi lời chúc đến bạn ',
+    btnReply: 'tôi biết bạn mệt mõi và áp lực lắp như mà cố lên nhé!!',
+    reply: 'Miệt mài suốt bao đêm thêu dệt những ước mơ, hãy biến ước muốn của bạn thành hiện thực. Thành công và hạnh phúc sẽ luôn đồng hành cùng bạn',
+    mess: 'tôi biết các bạn mệt mõi mà áp lức lặp như mà cố lên !!',
+    messDesc: 'còn vài hôm nữa thôi!!.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
